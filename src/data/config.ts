1f2c7f1ee3eb0a9d0009c4a49f8737c0b2c07f65
ada-zl5925 <zl5825@ic.ac.uk>
2026-04-02 18:33:53 +0100

    chore: update event dates in site configuration and story nodes to reflect new timeline

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  metDate: "2024-10-01",
+  metDate: "2026-03-06",
   entryTitle: "打开属于我们的故事",
   entryButton: "开始",
   proposalQuestion: "你愿意和我一起，把以后的时间也写进去吗？",
