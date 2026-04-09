d4dcff74833773baffdb3a0c0e68c9fcecba5e59
ada-zl5925 <zl5825@ic.ac.uk>
2026-04-09 06:08:52 +0100

    chore: update allowed development origins in configuration for improved local testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.228"],
+  allowedDevOrigins: ['10.76.39.168'],
 };
 
 export default nextConfig;
